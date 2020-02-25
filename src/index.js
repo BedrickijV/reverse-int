c32135e2c7757279e7d79a1abd1421e247deae33
Valera Bedrickij <becrickijv@gmail.com>
2020-02-25 09:41:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n > 0 ) {
+        let i = n.toString().split('').reverse().join('');
+        return Number(i)
+    } else {
+        let j = n.toString().split('');
+        j.shift();
+        return Number(j.reverse().join(''));
+    }
+};
